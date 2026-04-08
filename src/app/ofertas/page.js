c9902cb97ec627a624559c3f70e47cf2f597b906
Eric Pinheiro <eric.pinheiro@quickdigital.com.br>
2026-04-07 22:33:13 -0300

    feat: cria a rota /ofertas

---
+++ src/app/ofertas/page.js
@@ -0,0 +1,9 @@
+import "@/styles/ofertas.scss";
+
+export default function OfertasPage() {
+  return (
+    <main className="ofertas">
+      <h1 className="ofertas__title">Ofertas da Semana</h1>
+    </main>
+  );
+}
