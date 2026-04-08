ee2cef2ce4843e11c0668307917d7914994c45af
Eric Pinheiro <eric.pinheiro@quickdigital.com.br>
2026-04-08 13:50:32 -0300

    feat: add banner and product grid layout

--- src/app/ofertas/page.js
+++ src/app/ofertas/page.js
@@ -1,9 +1,21 @@
 import "@/styles/ofertas.scss";
+import Banner from "@/components/Banner";
 
 export default function OfertasPage() {
   return (
     <main className="ofertas">
+      <Banner />
+
       <h1 className="ofertas__title">Ofertas da Semana</h1>
+
+      <div className="ofertas__grid">
+        <div>Produto 1</div>
+        <div>Produto 2</div>
+        <div>Produto 3</div>
+        <div>Produto 4</div>
+        <div>Produto 5</div>
+        <div>Produto 6</div>
+      </div>
     </main>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,11 @@
+export default function Banner() {
+  return (
+    <div className="banner">
+      <img
+        className="banner__image"
+        src="https://images.unsplash.com/photo-1607082348824-0a96f2a4b9da"
+        alt="Banner de ofertas"
+      />
+    </div>
+  );
+}
