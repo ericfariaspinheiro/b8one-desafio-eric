7b6fc066d79a1be322c8ff2c2533c5e727d6ef20
Eric Pinheiro <eric.pinheiro@quickdigital.com.br>
2026-04-08 14:49:47 -0300

    feat: ativa ISR

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,7 +1,12 @@
 import ProductCard from "@/components/ProductCard";
 
 async function getProducts() {
-  const response = await fetch("https://fakestoreapi.com/products");
+  const response = await fetch(
+    "https://fakestoreapi.com/products",
+    {
+      next: { revalidate: 60 }
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Erro ao buscar produtos");
