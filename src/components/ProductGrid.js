b2b26329aac32127901151829c1aac9d09b7ef6d
Eric Pinheiro <eric.pinheiro@quickdigital.com.br>
2026-04-08 15:03:25 -0300

    fix: adiciona tratamento de erro na chamada da API

--- src/components/ProductGrid.js
+++ src/components/ProductGrid.js
@@ -1,21 +1,21 @@
 import ProductCard from "@/components/ProductCard";
 
 async function getProducts() {
-  const response = await fetch(
-    "https://fakestoreapi.com/products",
-    {
+  try {
+    const response = await fetch("https://fakestoreapi.com/products", {
       next: { revalidate: 60 }
-    }
-  );
+    });
 
-  if (!response.ok) {
-    throw new Error("Erro ao buscar produtos");
+    if (!response.ok) return [];
+    
+    const data = await response.json();
+    return data.slice(0, 6);
+  } catch (error) {
+    console.error(error);
+    return [];
   }
-
-  const data = await response.json();
-
-  return data.slice(0, 6);
 }
+
 
 export default async function ProductGrid() {
   const products = await getProducts();
